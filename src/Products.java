698497f98e08b913b576dc08d4bee69769ff848e
Abhishek <abhishekkatwork02@gmail.com>
2025-12-30 15:18:39 +0530

    Implemented user registration with file persistence

---
+++ src/Products.java
@@ -0,0 +1,13 @@
+public class Products {
+    String title;
+    double price;
+    String category;
+    String seller;
+
+    Products(String title, double price, String category, String seller){
+        this.title = title;
+        this.price = price;
+        this.category = category;
+        this.seller = seller;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,14 @@
+public class User {
+    String username;
+    String password;
+
+    User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString(){
+        return username + "," + password;
+    }
+}
